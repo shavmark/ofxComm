0ba5e854f9c7f8f92e53748c050d9afa3276a0f2
mark <mark@shavlik.us>
2016-12-16 09:40:56 -0600

    baseline

---
+++ src/networking.cpp
@@ -0,0 +1,527 @@
+#include "networking.h"
+
+// input buffer returned as reference
+bool compress(const char*buffer, size_t len, string&output) {
+	output.clear();
+	output.append(buffer, len);
+	return true; //bugbug restore compression when things mostly work
+	//bug remove one more data point
+	//bugbug add snappy and ssl of some kind size_t size = snappy::Compress((const char*)buffer, len, &output);
+	//bugbugif (size <= 0) {
+	//bugbug	ofLogError("compress") << "fails " << size;
+	//bugbug}
+	//bugbugreturn size > 0;
+}
+
+// input buffer returned as reference
+bool uncompress(const char*buffer, size_t len, string&output) {
+	output.clear();
+	output.append(buffer, len);
+	return true;
+///	bugbug add compress/ssl(in of) if (!snappy::Uncompress(buffer, len, &output)) {
+	//	ofLogError("uncompress") << "fails";
+		//return false;
+	//}
+	return true;
+}
+void UDPReceive::setup(int port){
+	reader.Create();
+	reader.Bind(port);
+	reader.SetNonBlocking(true);
+	startThread();
+}
+int UDPReceive::receive(string&message) {
+	message.resize(1000);//bugbug what is a good size?
+	return reader.Receive(&message[0], message.size());
+}
+// get a message from the qeueue
+bool UDPReceive::update(string&message) {
+	if (q.size() > 0) {
+		lock();
+		message = q.front();
+		q.pop_front();
+		unlock();
+		// from http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+		//rowMulti = find_if(_matrix.begin(), _matrix.end(),
+		//[=](const Node& n) {return n.i == i && n.j == k; });
+		message.erase(std::find_if(message.rbegin(), message.rend(),
+			std::not1(std::ptr_fun<int, int>(std::isspace))).base(), message.end());
+		size_t len = strlen(message.c_str()); // ignore trailing 0s and white space, assume json is in the string
+		message.resize(len);
+		return true;
+	}
+	return false;
+}
+void UDPReceive::threadedFunction() {
+	string message;
+
+	while (1) {
+		if (receive(message)) {
+			lock();
+			q.push_back(message); // save a copy at the end of the list FIFO
+			unlock();
+			yield();
+		}
+	}
+}
+void Serial::setup() {
+		data.setup(0, 38400);
+		//Parity : None
+		//Data Bits : 8
+		//Stop Bits : 1
+}
+void UDPBroadcast::setup(int port) {
+	sender.Create();
+	sender.Connect("192.168.1.255", port);//broadcast back to everyone
+	sender.SetNonBlocking(true);
+	startThread();
+}
+void UDPBroadcast::update(const string&message) {
+	lock();
+	q.push_back(message); //bugbug do we want to add a priority? front & back? not sure
+	unlock();
+}
+int UDPBroadcast::send(const string&message) {
+	return sender.Send(message.c_str(), message.length());
+}
+
+void UDPBroadcast::threadedFunction() {
+	while (1) {
+		lock();
+		if (q.size() > 0) {
+			send(q.front());
+			q.pop_front();
+		}
+		unlock();
+		yield();
+	}
+}
+
+string getRemoteIP(shared_ptr<ofxOscMessage>m) {
+	if (m) {
+		return m->getRemoteIp();
+	}
+	return "";
+}
+
+void getRawString(string &buffer, shared_ptr<ofxOscMessage>m) {
+	buffer.clear();
+	if (m) {
+		string input = m->getArgAsString(0);
+		uncompress(input.c_str(), input.size(), buffer);
+	}
+	}
+
+shared_ptr<ofxJSON> toJson(shared_ptr<ofxOscMessage> m) {
+	if (m) {
+		// bugbug data comes from one of http/s, string or local file but for now just a string
+		shared_ptr<ofxJSON> p = std::make_shared<ofxJSON>();
+		if (p) {
+			string output;
+			string input = m->getArgAsString(0);
+			uncompress(input.c_str(), input.size(), output);
+			p->parse(output); // uncompress returns input upon error
+		}
+		return p;
+	}
+	return nullptr;
+}
+shared_ptr<ofxOscMessage> fromJson(ofxJSON &data, const string&address) {
+	shared_ptr<ofxOscMessage> p = std::make_shared<ofxOscMessage>();
+	if (p) {
+		p->setAddress(address); // use 32 bits so we can talk to everyone easiy
+								//bugbug if these are used find a way to parameterize
+								//bugbug put all these items in json? or instead use them
+								// to ignore messages, delete old ones?
+		// even compress the small ones so more messages can use udp
+		string output;
+		string input = data.getRawString(false);
+		compress(input.c_str(), input.size(), output);
+		p->addStringArg(output); // all data is in json
+	}
+	return p;
+}
+void WriteOsc::setup(const string &hostname, int port) {
+	sender.setup(hostname, port);
+	startThread();
+}
+void WriteOsc::threadedFunction() {
+	while (1) {
+		lock();
+		if (q.size() > 0) {
+			shared_ptr<ofxOscMessage> m = q.front();
+			q.pop_front();
+			if (m) {
+				sender.sendMessage(*m, false);
+			}
+		}
+		unlock();
+		yield();
+	}
+}
+// return true to ignore messages that have been added recently
+bool WriteOsc::ignoreDups(shared_ptr<ofxOscMessage> p, ofxJSON &data, const string&address) {
+	// only add if its not in the list already 
+	for (int i = 0; i < memory.size(); ++i) {
+		if (memory[i]->getAddress() == address && memory[i]->getArgAsString(0) == data.getRawString()) {
+			return true; // ignore dup
+		}
+	}
+	memory.push_front(p);
+	if (memory.size() > 1000) {
+		memory.erase(memory.end() - 200, memory.end());// find to the 1000 and 200 magic numbers bugbug
+	}
+	return false;
+}
+void WriteOsc::send(shared_ptr<ofxOscMessage> m, const string&address) {
+	if (m) {
+		m->setAddress(address);
+		lock();//bugbug review useage of lock in entire app
+		q.push_front(m); //bugbub do we want to add a priority? front & back? not sure
+		unlock();
+	}
+}
+// simples string send
+void WriteOsc::send(const string&data, const string&address) {
+	if (data.size() > 0) {
+		shared_ptr<ofxOscMessage> p = std::make_shared<ofxOscMessage>();
+		if (p) {
+			p->setAddress(address); 
+			string output;
+			if (compress(data.c_str(), data.size(), output)) {
+				p->addStringArg(output); 
+				lock();
+				q.push_front(p); //bugbub do we want to add a priority? front & back? not sure
+				unlock();
+			}
+		}
+	}
+
+}
+
+// add a message to be sent, json is default
+void WriteOsc::send(ofxJSON &data, const string&address) {
+	if (data.size() > 0) {
+		shared_ptr<ofxOscMessage> p = fromJson(data, address);
+		if (p) {
+			if (checkForDups && ignoreDups(p, data, address)) {
+				return;
+			}
+			lock();
+			q.push_front(p); //bugbub do we want to add a priority? front & back? not sure
+			unlock();
+		}
+	}
+}
+void ReadOsc::setup(int port) {
+	receiver.setup(port);
+	startThread();
+}
+
+void ReadOsc::threadedFunction() {
+	while (1) {
+		// check for waiting messages
+		while (receiver.hasWaitingMessages()) {
+			shared_ptr<ofxOscMessage> p = std::make_shared<ofxOscMessage>();
+			if (p) {
+				lock();
+				receiver.getNextMessage(*p);
+				q[p->getAddress()].push_front(p); // will create a new queue if needed
+				unlock();
+			}
+			yield();
+		}
+	}
+}
+shared_ptr<ofxOscMessage> ReadOsc::getMessage(const string&address) {
+	shared_ptr<ofxOscMessage> m = nullptr;
+	lock();
+	if (q.size() > 0) {
+		MessageMap::iterator found = q.find(address);
+		if (found != q.end() && found->second.size() > 0) {
+			m = (found->second).back();
+			found->second.pop_back();// first in first out
+		}
+	}
+	unlock();
+
+	return m;
+}
+string ReadOsc::getString(string &buffer, const string&address) {
+	string sourceIP;
+	lock();
+	if (q.size() > 0) {
+
+		MessageMap::iterator found = q.find(address);
+		if (found != q.end() && found->second.size() > 0) {
+			shared_ptr<ofxOscMessage> p = (found->second).back();
+			getRawString(buffer, p);
+			found->second.pop_back();// first in first out
+			sourceIP = getRemoteIP(p);
+		}
+	}
+	unlock();
+	return sourceIP;
+}
+shared_ptr<ofxJSON> ReadOsc::getJson(const string&address) {
+	shared_ptr<ofxJSON> j = nullptr;
+	lock();
+	if (q.size() > 0) {
+		MessageMap::iterator found = q.find(address);
+		if (found != q.end() && found->second.size() > 0) {
+			shared_ptr<ofxOscMessage> p = (found->second).back();
+			if (p && !p->getArgAsBool(0)) {//bugbug this code is not tested
+				j = toJson(p);
+			}
+			found->second.pop_back();// first in first out
+		}
+	}
+	unlock();
+	return j;
+}
+
+void TCPServer::setup(int port, bool blocking) {
+	//11999
+	server.setup(port, blocking);
+	startThread();
+}
+void TCPMessage::setup(size_t bytesToSend, DataType typeIn, TypeOfSend typeOfSendIn, int clientIDIn) {
+	clientID = clientIDIn;
+	typeOfSend = typeOfSendIn;
+	packet.typeOfPacket = typeIn; // passed as a double check
+	numberOfBytesToSend = bytesToSend;
+	packet.b[0] = PacketFence; // help check for lost or out of sync data
+	pixels = nullptr;
+}
+
+// input data is  deleted by this object at the right time (at least that is the plan)
+void TCPServer::update(const char * bytes, const size_t numBytes, DataType type, TypeOfSend typeOfSend, int clientID) {
+	string buffer;
+	if (compress(bytes, numBytes, buffer)) { // copy and compress data so caller can free passed data 
+		char *bytes = new char[sizeof(TCPMessage) + buffer.size()];
+		if (bytes) {
+			memset(bytes, 0, sizeof(TCPMessage) + buffer.size());
+			TCPMessage *message = (TCPMessage *)bytes;
+			message->setup(sizeof(TCPPacket) + buffer.size(), type, typeOfSend, clientID);
+			memcpy_s(&message->packet.b[1], buffer.size(), buffer.c_str(), buffer.size());
+			lock();
+			if (q.size() > maxItems) {
+				q.pop_back(); // remove oldest
+			}
+			q.push_front(message); //bugbub do we want to add a priority? 
+			unlock();
+		}
+	}
+	return;
+}
+// no compression yet bugbug
+void TCPServer::update(shared_ptr<ofPixels>pixels, DataType type, TypeOfSend typeOfSend, int clientID) {
+	unsigned char *data = pixels->getPixels();
+	TCPMessage *message = new TCPMessage;
+	if (message && data) {
+		message->setup(0, type, typeOfSend, clientID);
+		message->pixels = pixels;
+		lock();
+		if (q.size() > maxItems) {
+			q.pop_back(); // remove oldest
+		}
+		q.push_front(message); //bugbub do we want to add a priority? 
+		unlock();
+	}
+	return;
+}
+
+// control data deletion (why we have our own thread) to avoid data copy since this code is in a Kinect crital path 
+void TCPServer::threadedFunction() {
+	while (1) {
+		lock();
+		if (q.size() > 0) {
+			TCPMessage* m = q.back();// last in first out
+			q.pop_back();
+			if (m) { 
+				if (m->typeOfSend == Stream) {
+					sendStream(m);
+					delete m;
+				}
+				else if (m->typeOfSend == Message) {
+					sendMessage(m);
+					delete (char *)m; // allocated as char buffer
+				}
+			}
+		}
+		unlock();
+		yield();
+	}
+}
+// client needs to know if its a stream of message
+void TCPServer::sendMessage(TCPMessage *m) {
+	if (m) {
+		if (m->numberOfBytesToSend > maxSend) {
+			ofLogError("TCPServer::sendbinary") << "block too large " << ofToString(m->numberOfBytesToSend) + " max " << ofToString(maxSend);
+			return;
+		}
+		if (server.getNumClients() > 0) {
+			if (m->clientID > 0) {
+				ofLogVerbose("TCPServer::sendRawMsg") << "bytes " << m->numberOfBytesToSend;
+				server.sendRawMsg(m->clientID, (const char*)&m->packet, m->numberOfBytesToSend);
+			}
+			else {
+				ofLogVerbose("TCPServer::sendRawMsgToAll") << "bytes " << m->numberOfBytesToSend;
+				server.sendRawMsgToAll((const char*)&m->packet, m->numberOfBytesToSend);
+			}
+		}
+	}
+}
+void TCPServer::sendStream(TCPMessage *m) {
+	if (server.getNumClients() > 0 && m && m->pixels) {
+		const char* index = (const char*)m->pixels->getPixels(); //start at beginning of pixel array 
+		int length = m->pixels->getWidth() * 3;//length of one row of pixels in the image 
+		int size = m->pixels->getHeight() * m->pixels->getWidth() * 3;
+		int pixelCount = 0;
+		while (pixelCount < size) {
+			if (m->clientID > 0) {
+				server.sendRawBytes(m->clientID, index, length); //bugbug not sure if different sizes go
+			}
+			else {
+				server.sendRawBytesToAll(index, length); //send the first row of the image 
+			}
+			index += length; //increase pointer so that it points to the next image row 
+			pixelCount += length; //increase pixel count by one row 
+		}
+	}
+}
+// default saves to queue
+void TCPPixels::myUpdate(shared_ptr<ofPixels> pixels) {
+	lock();
+		q.push_back(pixels);
+	unlock();
+}
+
+//Receiving loop that must ensure a frame is received as a whole
+// must know the size of item being sent
+void TCPPixels::update() {
+	//bugbug getting too many pixels but we do not care
+	int length = width * 3;
+	int totalReceivedBytes = 0;
+	int size = (width * height * 3)+sizeof(TCPPacket);
+	char* start;
+	char* receivePos = start = new char[size*2];
+	if (start) {
+		shared_ptr<ofPixels> pixels = std::make_shared<ofPixels>();
+		if (pixels) {
+			pixels->allocate(width, height, OF_IMAGE_COLOR);
+			while (totalReceivedBytes < size) {
+				int receivedBytes = tcpClient.receiveRawBytes(receivePos, length); //returns received bytes 
+				if (receivedBytes < 0) {
+					break; // try again later
+				}
+				for (int i = 0; i < receivedBytes; ++i) {
+			//		(*pixels)[i + totalReceivedBytes] = receivePos[i];
+				}
+				totalReceivedBytes += receivedBytes;
+				receivePos += receivedBytes;
+			}
+			ofLogNotice("TCPPixels::update") << "receiveRawBytes packet size " << size << " type Pixels total:" << totalReceivedBytes;
+			lock();
+			myUpdate(pixels);
+			// not used q.push_back(pixels);
+			unlock();
+		}
+		delete start;
+	}
+
+}
+void TCPClient::myUpdate(shared_ptr<ReadTCPPacket> packet) {
+	q.push_back(packet); //bugbug assume called while locked but can we just lock here vs in update?
+}
+
+void TCPClient::update() {
+	char* b = (char*)std::malloc(maxSend);
+	if (b) {
+		int messageSize;
+		lock(); // big lock window
+		// this api will write the size of the data not the size of the buffer we pass in (ouch)
+		// it buffers data beteen its markets and returns 0 until all data between 
+		// markers is in its buffer which it then returns.
+		messageSize = tcpClient.receiveRawMsg(b, maxSend);
+		// only occurs due to bug or hack as we never send more than MAXSEND ourself, at this point we need to crash
+		if (messageSize > maxSend) {
+			ofExit(-2); // enables DOS, the real fix is for to not over flow in receiveRawMsg bugbug
+		}
+		if (messageSize > 0) {
+			TCPPacket*p = (TCPPacket*)b;
+			if (p->b[0] == PacketFence) { // basic validation
+				shared_ptr<ReadTCPPacket> returnedData = std::make_shared<ReadTCPPacket>();
+				if (returnedData) {
+					if (uncompress(&p->b[1], messageSize - sizeof(TCPPacket), returnedData->data)) {
+						returnedData->typeOfPacket = p->typeOfPacket;
+						ofLogVerbose("TCPClient::update") << "receiveRawMsg packet size " << messageSize << " type " << p->typeOfPacket;
+						myUpdate(returnedData);
+					}
+					else {
+						ofLogError("TCPClient::update") << "data ignored";
+					}
+				}
+			}
+		}
+		unlock();
+		free(b); // only delete if data not returned
+	}
+	return;
+}
+
+void Server::setup() {
+	comms.setup();
+}
+void Server::addTCPServer(OurPorts port, bool blocking) {
+	shared_ptr<TCPServer> s = std::make_shared<TCPServer>();
+	if (s) {
+		s->setup(port, blocking);
+		servers[port] = s; // will create a new queue if needed
+	}
+}
+DataType mapPortToType(OurPorts port) {
+	switch (port) {
+	case TCP:
+		return TCPID;
+	case TCPKinectIR:
+		return IrID;
+	case TCPKinectBodyIndex:
+		return BodyIndexID;
+	case TCPKinectBody:
+		return BodyID;
+	default:
+		ofLogError("mapPortToType") << "invalid port " << port;
+		return UnknownID;
+	}
+}
+
+void Server::sendTCP(const char * bytes, const size_t numBytes, OurPorts port, TypeOfSend typeOfSend, int clientID) {
+	if (numBytes > 0) {
+		ServerMap::const_iterator s = servers.find(port);
+		if (s != servers.end()) {
+			s->second->update(bytes, numBytes, mapPortToType(port),  typeOfSend, clientID);
+		}
+	}
+}
+void Server::sendTCP(shared_ptr<ofPixels>pixels, OurPorts port, TypeOfSend typeOfSend, int clientID) {
+	if (pixels->size() > 0) {
+		ServerMap::const_iterator s = servers.find(port);
+		if (s != servers.end()) {
+			s->second->update(pixels, mapPortToType(port), typeOfSend, clientID);
+		}
+	}
+}
+
+bool Server::tcpEnabled() {
+	ServerMap::const_iterator s = servers.find(TCP);
+	return s != servers.end();
+}
+bool Server::enabled(OurPorts port) {
+	ServerMap::const_iterator s = servers.find(port);
+	if (s != servers.end() && s->second.get()) {
+		return s->second.get()->clientCount()  > 0;
+	}
+	return false;
+}
+
---
+++ src/networking.h
@@ -0,0 +1,253 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
+#include "ofxJSON.h"
+#include "ofxOsc.h"
+#include "ofxNetwork.h"
+
+using namespace std;
+
+//how to broad cast https://forum.openframeworks.cc/t/fixed-ofxosc-broadcast-to-255-255-255-255-in-of-0-84-under-windows/21974
+//https://github.com/microcosm/KinectV2-OSC Address: /bodies/{bodyId}/joints/{jointId}
+//Values: -float : positionX
+	//- float : positionY
+	//- float : positionZ
+	//- string : trackingState(Tracked, NotTracked or Inferred)
+
+
+// phase II go to something like IoTivity or AllJoyn, let the dust settle.  
+
+// notes: remote files can use ofImage.loadImage("http://some.com/url.png")
+
+	static const string defaultServerIP = "192.168.1.25";//bugbug working to make this dynamic
+	static const string SignOnKinectServerOscAddress="storyteller/kinect/server/signon";
+	static const string SignOnClientOscAddress = "storyteller/client/signon";
+	static const int16_t maxSend = 1024 * 1024;
+
+	enum DataType : char {
+		TCPID = 't',	//116
+		BodyIndexID = 'x',//120
+		IrID = 'i',		//105
+		BodyID = 'b',	// 98
+		JsonID = 'j',	//106
+		Pixels = 'p',		//12
+		UnknownID = 'k'
+	};
+	enum TypeOfSend : char {
+		Stream = 's',
+		Message = 'm',
+		UnknownSend = '?'
+	};
+	enum OurPorts : int {
+		OSC = 2552, 
+		TCP, // generic TCP
+		UDP, // generic UDP
+		TCPKinectIR,
+		TCPKinectBodyIndex,
+		TCPKinectBody,
+		UDPSend = 11002,
+		UDPRECEIVE = 11000
+	};
+
+	bool compress(const char*buffer, size_t len, string&output);
+	bool uncompress(const char*buffer, size_t len, string&output);
+	shared_ptr<ofxJSON> toJson(shared_ptr<ofxOscMessage>);
+	shared_ptr<ofxOscMessage> fromJson(ofxJSON &data, const string&address);
+	void getRawString(string &buffer, shared_ptr<ofxOscMessage>);
+	string getRemoteIP(shared_ptr<ofxOscMessage>m);
+	DataType mapPortToType(OurPorts port);
+
+	const char PacketFence = 'f'; // used to validate packet were set properly
+
+	// what gets sent over the wire
+	struct TCPPacket {
+		DataType typeOfPacket; // bytes only
+		char b[1]; // validates data was properly read
+	};
+	struct ReadTCPPacket {
+		DataType typeOfPacket; // byte only
+		string data;
+	};
+
+	class Serial {
+	public:
+		void setup();
+		ofSerial data;
+	};
+	class TCPMessage {
+	public:
+		void setup(size_t bytesToSend, DataType type, TypeOfSend typeOfSend=Message, int clientID = -1);
+		
+		int clientID;	// -1 for all connected
+		TypeOfSend typeOfSend;// 's' stream or 'm' message
+		shared_ptr<ofPixels> pixels; // optional pixels to send
+		size_t numberOfBytesToSend;
+		TCPPacket packet;		// data that is sent
+	};
+
+	class UDPReceive : public ofThread {
+	public:
+		void setup(int port = UDPRECEIVE);
+		bool update(string&message);
+		ofxUDPManager reader;
+	private:
+		int receive(string&message);
+		void threadedFunction();
+		deque<string> q; // message queue
+	};
+
+	class UDPBroadcast : public ofThread {
+	public:
+		void setup(int port = UDPSend);
+		void update(const string&message);
+		ofxUDPManager sender;
+	private:
+		int send(const string&message);
+		void threadedFunction();
+		deque<string> q; // message queue
+	};
+
+	// deque allows push front and back and enumration so we do priorities and remove old data
+	typedef std::unordered_map<string, deque<shared_ptr<ofxOscMessage>>>MessageMap;
+
+	class WriteOsc : public ofThread {
+	public:
+		void setup(const string &hostname = "192.168.1.255", int port = OurPorts::OSC);
+
+		// add a message to be sent
+		void send(ofxJSON &data, const string&address);
+		void send(const string&data, const string&address);
+		void send(shared_ptr<ofxOscMessage>, const string&address);
+	private:
+		void threadedFunction();
+		bool ignoreDups(shared_ptr<ofxOscMessage> p, ofxJSON &data, const string&address);
+		bool checkForDups = false;
+		ofxOscSender sender;
+		deque<shared_ptr<ofxOscMessage>> q;
+		deque<shared_ptr<ofxOscMessage>> memory; // used to avoid dups, saves last 200 messages bugbug check this number
+	};
+
+	class ReadOsc : public ofThread {
+	public:
+		void setup(int port = OSC);
+
+		// caller can use the address to determine how things are sent
+		shared_ptr<ofxJSON> getJson(const string&address);
+		// returns source ip
+		string getString(string &buffer, const string&address);
+		shared_ptr<ofxOscMessage> getMessage(const string&address);
+	private:
+		void threadedFunction();
+		ofxOscReceiver receiver;
+		MessageMap q;
+	};
+
+	class TCPServer : public ofThread {
+	public:
+		void setup(int port= TCP, bool blocking = false);
+		int  port() { return server.getPort(); }
+		int  clientCount() { return server.getNumClients(); }
+		void update(const char * rawBytes, const size_t numBytes, DataType type, TypeOfSend typeOfSend=Message, int clientID = -1);
+		void update(shared_ptr<ofPixels>pixels, DataType type, TypeOfSend typeOfSend=Stream, int clientID=-1);
+		int maxItems = 10; // max size of q
+	private:
+		ofxTCPServer server;
+		void threadedFunction();
+		void sendMessage(TCPMessage *m);
+		void sendStream(TCPMessage *m);
+		deque<TCPMessage*> q;
+	};
+	template <class T>
+	class TCPBase : public ofThread {
+	public:
+		shared_ptr<T> get() {
+			shared_ptr<T>p = nullptr;
+			lock(); // q can pop in an other thread then q.back will fail
+			if (q.size() > 0) {
+				p = q.back();// last in first out
+				q.pop_back();
+			}
+			unlock();
+			return p;
+		}
+		void setup(const string& ipIn, int portIn, bool blockingIn) {//192.168.1.21 or 127.0.0.1
+			ip = ipIn; // save to retry setup via thread
+			port = portIn;
+			blocking = blockingIn;
+			if (!tcpClient.isConnected()) {
+				if (!tcpClient.setup(ip, port, blocking)) {
+					ofSleepMillis(500); // wait before a try again if failed
+					tcpClient.setup(ip, port, blocking); // caller must try again beyond this
+				}
+				if (tcpClient.isConnected()) {
+					ofLogNotice("TCPClient::setup") << "connected " << ip << ":" << port << " blocking" << blocking;
+					if (!isThreadRunning()) {
+						startThread(); // start thread once connection is made
+					}
+				}
+			}
+		}
+
+	protected:
+		ofxTCPClient tcpClient;
+		deque<shared_ptr<T>> q; // optional q
+	private:
+		void setup() { setup(ip, port, blocking); }// enables setup retry in the thread loop, not for external callers
+		void threadedFunction() {
+			while (1) {
+				if (tcpClient.isConnected()) {
+					update();
+				}
+				else {
+					setup();
+				}
+				yield();
+			}
+		}
+		virtual void update()=0;
+		string ip; // save to retry setup via thread
+		int port=0;
+		bool blocking=true;
+	};
+
+	class TCPClient : public TCPBase<ReadTCPPacket> {
+	public:
+	protected:
+		void update();
+		virtual void myUpdate(shared_ptr<ReadTCPPacket> packet);// let dervied class do their thing, q not used
+	private:
+	};
+	class TCPPixels : public TCPBase<ofPixels> {
+	public:
+	// ex: ofTexture::readToPixels(pixels); // now all the pixels from tex are in pix
+		float width;//bugbug tie back to kinect = getDepthFrameWidth();
+		float height;//bugbug tie back to kinect = getDepthFrameHeight();
+	protected:
+		void update();
+		virtual void myUpdate(shared_ptr<ofPixels> pixels);// let dervied class do their thing, q not used
+	private:
+	};
+
+	typedef std::unordered_map<OurPorts, shared_ptr<TCPServer>> ServerMap;
+
+	// send in tpc or osc, server may be a bit too complicated since it trys to be one server source for all things, not sure yet
+	class Server {
+	public:
+		void setup();
+		void sendTCP(shared_ptr<ofPixels>pixels, OurPorts port, TypeOfSend typeOfSend=Stream, int clientID=-1);
+		void sendTCP(const char * bytes, const size_t numBytes, OurPorts port, TypeOfSend typeOfSend = Message, int clientID = -1);
+		void addTCPServer(OurPorts port = TCP, bool blocking = false);
+		bool tcpEnabled();
+		bool enabled(OurPorts port);
+		void sendOsc(ofxJSON &data, const string&address) { comms.send(data, address); }
+		void sendOsc(const string &data, const string&address) { comms.send(data, address); }
+		void sendOsc(shared_ptr<ofxOscMessage>data, const string&address) { if (data) comms.send(data, address); }
+	private:
+		WriteOsc comms;
+		ServerMap servers;
+	};
